7c4afb2727f74edba4d25b9f8c2e36f969e0f12d
Steve Etherington <s_etherington@hotmail.com>
2020-02-16 12:29:06 +0000

    reversals

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      reverseR,
+      reverseF
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+reverseR :: [a] -> [a]
+reverseR xs = reverse2  xs [] 
+    where reverse2  [] rev = rev
+          reverse2  (x:xs) ys = reverse2  xs (x:ys)
+
+reverseF :: [a] -> [a]
+reverseF list = foldl (\xs -> \x -> x:xs) [] list          
